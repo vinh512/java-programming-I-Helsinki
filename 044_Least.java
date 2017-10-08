69c541f0e03da56c15ccd096430ccc55a9a4c625
vinh huynh <vvvhuynh@yahoo.com>
2017-10-08 14:41:10 -0500

    add Least solution

---
+++ 044_Least.java
@@ -0,0 +1,15 @@
+public class Least {
+
+    public static int least(int number1, int number2) {
+    	
+        if (number1 > number2) {
+            return number2;
+        } else 
+            return number1;      
+    }
+
+    public static void main(String[] args) {
+        int result = least(2, 7);
+        System.out.println("Least: " + result);
+    }
+}
