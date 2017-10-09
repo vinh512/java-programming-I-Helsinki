44d68cc6566ef3ca0792db1ab5cb1f99a20c65d0
vinh huynh <vvvhuynh@yahoo.com>
2017-10-09 17:51:37 -0500

    add LastCharacter solution

---
+++ 049_LastCharacter.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LastCharacter {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String name = reader.nextLine();
+        
+        System.out.println("Last character: " + lastCharacter(name));
+    }
+    
+    public static char lastCharacter(String name) {
+        return name.charAt(name.length() - 1);
+    }    
+}
