02ef98acdebe24f13ea93687fb79ae1b987493eb
vinh huynh <vvvhuynh@yahoo.com>
2017-10-12 22:41:36 -0500

    add SeparatingCharacters solution

---
+++ 051_SeparatingCharacters.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SeparatingCharacters {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String name = reader.nextLine();
+        
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println((i+1) + ". character: " + name.charAt(i));
+        }
+    }
+}
