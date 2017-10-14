d3abe4337ca99271a8c0abb7681e24d0c379dfc7
vinh huynh <vvvhuynh@yahoo.com>
2017-10-13 23:45:33 -0500

    add comments to clarify

--- 052_ReversingName.java
+++ 052_ReversingName.java
@@ -9,6 +9,13 @@ public class ReversingName {
         String name = reader.nextLine();
         
         System.out.print("In reverse order: " );
+        
+        // - We initialize i to the length of the name but we need to subtract
+        //   1 because of 0-based indexing
+        // - We then check to see if the index is greater than zero which means
+        //   that there are still letters of the name to iterate through
+        // - Then print out each char at the given index
+        // - Lastly, we decrement
         for (int i = name.length()-1; i >= 0 ; i--) {
             System.out.print(name.charAt(i));
         }
