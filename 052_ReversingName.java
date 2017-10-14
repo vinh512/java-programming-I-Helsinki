0ced72fc9b50d4d0bce380e50c9181b058f87b0c
vinh huynh <vvvhuynh@yahoo.com>
2017-10-13 23:41:08 -0500

    add ReversingName solution

---
+++ 052_ReversingName.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ReversingName {
+	
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        
+        System.out.print("In reverse order: " );
+        for (int i = name.length()-1; i >= 0 ; i--) {
+            System.out.print(name.charAt(i));
+        }
+    }
+}
