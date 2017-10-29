860f574f1671dcde21f2cfff4fd97f0558136c0d
vinh huynh <vvvhuynh@yahoo.com>
2017-10-28 20:13:16 -0500

    add AverageOfNumbers solution

---
+++ 064_AverageOfNumbers.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class AverageOfNumbers {
+
+    public static int sum(ArrayList<Integer> list) {
+        int sum = 0;
+        for (Integer val : list) {
+            sum += val;
+        }
+        return sum;
+    }
+    
+    public static double average(ArrayList<Integer> list) {        
+        double average = 0.0;
+        average = (double) sum(list) / list.size();
+        return average;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(2);
+        list.add(7);
+        list.add(2);
+
+        System.out.println("The average is: " + average(list));
+    }
+}
